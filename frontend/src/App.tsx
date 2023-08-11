ccd22ac15d82284551ab43786a6c7160597ec8e0
Matt Simon <109586994+MattSimon24@users.noreply.github.com>
2023-08-11 11:34:52 -0600

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,39 @@
+
+import { useEffect, useState } from 'react'
+import { Note as NoteModel } from './models/note';
+import './App.css'
+import Note from './components/Note';
+import { Container, Row } from 'react-bootstrap';
+
+function App() {
+  const [notes, setNotes] = useState<NoteModel[]>([]);
+
+  useEffect(() => {
+    const loadNotes= async() =>  {
+      try {
+        const response = await fetch("/api/notes", { method: "GET" });
+        const notes = await response.json();
+        setNotes(notes);
+      } catch (error) {
+        console.error(error);
+        // alert(error);
+      }
+    }
+    loadNotes();
+  }, []);
+
+  return (
+    <>
+      <Container>
+        <Row xs={1} md={2} xl={3} className="g-4">
+        {notes.map(note => (
+          <Note note = {note} key={note._id} />
+        ))}
+
+        </Row>
+      </Container>
+    </>
+  )
+}
+
+export default App
